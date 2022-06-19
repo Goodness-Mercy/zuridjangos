04c6ed26d4dbb29883424f6cc63b0d5b4ef9e96e
Goodnessmercy <goodiemercy758@gmail.com>
2022-06-19 14:28:27 +0100

    "my first commit"

---
+++ GoodnessNwaobilo/GoodnessNwaobilo/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for GoodnessNwaobilo project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GoodnessNwaobilo.settings')
+
+application = get_asgi_application()
